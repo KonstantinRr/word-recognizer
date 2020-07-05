33813e520d150e4fb431e0994e79b21a9f342554
Konstantin Rolf <konstantin.rolf@gmail.com>
2020-07-05 16:39:16 +0200

    Initialized start file

---
+++ recognizer.py
@@ -0,0 +1,12 @@
+# Neural Networks - UNIVERSITY OF GRONINGEN - 2020
+# Konstantin Rolf - S3750558
+# Kacper - (SNumber)
+# Nicholas Koundouros - (SNumber)
+# Daniel - (SNumber)
+
+import tensorflow as tf
+import numpy as np
+
+
+if __name__ == '__main__':
+    print('Runnning Word recognizer')
